edb26cab1d04df0b542da78887e0d73b54d071ca
Parker Mathis <parkerjmathis@gmail.com>
2022-11-02 18:30:06 -0700

    Store value from text entry

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,12 @@
 var searchFormEl = $("#search-form");
+var cityInputVal = "";
+var searchButtonEl = $("#search-button");
 
-function handleSearchSubmit(event) {
+searchButtonEl.on("click", function (event) {
   event.preventDefault();
-
-  var cityInputVal = $("#city-input");
-  if (!cityInputVal) {
-    return;
-  }
-}
+  cityInputVal = $("#city-input").val();
+  console.log(cityInputVal);
+});
 
 var requestUrl =
   "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=7e84530e4048780b769d94acf3761dbf";
