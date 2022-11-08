5b27ac0389ba39a16a15c0050a64e4ebd01196f1
Parker Mathis <parkerjmathis@gmail.com>
2022-11-07 18:16:35 -0800

    Bug fixes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,7 @@
 var cityInputVal = "";
+var cityListVal = "";
 var citySearchListEl = document.querySelector("#citySearchList");
-var buttonsEl = document.getElementsByClassName("city-input-button");
+var searchButton = document.querySelector("#search-button");
 var forecastFull = [];
 var cityList = [];
 
@@ -9,12 +10,15 @@ function renderCityList() {
   for (var i = 0; i < cityList.length; i++) {
     var citySearch = cityList[i];
     var newButton = document.createElement("button");
-    newButton.classList.add("city-input-button");
+    newButton.classList.add("city-input");
     newButton.classList.add("btn-block");
     newButton.textContent = citySearch;
     newButton.style.display = "block";
     newButton.value = citySearch;
     citySearchListEl.appendChild(newButton);
+    newButton.addEventListener("click", function (event) {
+      handleSearch(event);
+    });
   }
 }
 
@@ -26,9 +30,14 @@ function init() {
   var storedRecentSearches = JSON.parse(localStorage.getItem("cityList"));
   if (storedRecentSearches !== null) {
     cityList = storedRecentSearches;
+    renderCityList();
   }
-  renderCityList();
+  searchButton.addEventListener("click", function (event) {
+    handleSearch(event);
+  });
 }
+
+init();
 
 function populateCards() {
   for (var i = 0; i < forecastFull.length; i++) {
@@ -64,47 +73,54 @@ function populateCards() {
   }
 }
 
-init();
+function handleSearch(event) {
+  event.preventDefault();
+  var buttonClicked = event.target;
+  cityListVal = buttonClicked.value;
+  cityInputVal = $(".city-input-text").val();
 
-for (var i = 0; i < buttonsEl.length; i++) {
-  buttonsEl[i].addEventListener("click", function (event) {
-    event.preventDefault();
-    var buttonClicked = event.target;
-    cityListVal = buttonClicked.value;
-    cityInputVal = $(".city-input-text").val();
-    if (cityInputVal === "" && cityListVal === "") {
-      return;
-    }
-    (async function () {
-      try {
-        const coordSearch = await fetch(
+  if (cityInputVal === "" && cityListVal === "") {
+    return;
+  }
+  (async function () {
+    try {
+      var coordSearch = "";
+      if (cityListVal) {
+        coordSearch = await fetch(
           "http://api.openweathermap.org/data/2.5/weather?q=" +
-            cityInputVal +
             cityListVal +
             "&units=imperial&appid=7e84530e4048780b769d94acf3761dbf"
         );
-        const coordSearchData = await coordSearch.json();
-        forecastFull[0] = coordSearchData;
-        const forecastSearch = await fetch(
-          `https://api.openweathermap.org/data/2.5/forecast?lat=${coordSearchData.coord.lat}&lon=${coordSearchData.coord.lon}&units=imperial&appid=7e84530e4048780b769d94acf3761dbf`
+      } else {
+        coordSearch = await fetch(
+          "http://api.openweathermap.org/data/2.5/weather?q=" +
+            cityInputVal +
+            "&units=imperial&appid=7e84530e4048780b769d94acf3761dbf"
         );
-        const forecastSearchData = await forecastSearch.json();
-        forecastFull[1] = forecastSearchData.list[6];
-        forecastFull[2] = forecastSearchData.list[14];
-        forecastFull[3] = forecastSearchData.list[22];
-        forecastFull[4] = forecastSearchData.list[30];
-        forecastFull[5] = forecastSearchData.list[38];
-
-        // Loop to populate the current forecast and five day forecast sections
-        populateCards();
-        if (cityInputVal !== "") {
-          cityList.push(cityInputVal);
-          storeCities();
-          renderCityList();
-        }
-      } catch (error) {
-        console.log("Error: " + error);
       }
-    })();
-  });
+
+      const coordSearchData = await coordSearch.json();
+      forecastFull[0] = coordSearchData;
+      const forecastSearch = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${coordSearchData.coord.lat}&lon=${coordSearchData.coord.lon}&units=imperial&appid=7e84530e4048780b769d94acf3761dbf`
+      );
+      const forecastSearchData = await forecastSearch.json();
+      forecastFull[1] = forecastSearchData.list[6];
+      forecastFull[2] = forecastSearchData.list[14];
+      forecastFull[3] = forecastSearchData.list[22];
+      forecastFull[4] = forecastSearchData.list[30];
+      forecastFull[5] = forecastSearchData.list[38];
+
+      // Loop to populate the current forecast and five day forecast sections
+      populateCards();
+      if (cityInputVal !== "") {
+        cityList.push(cityInputVal);
+        storeCities();
+        renderCityList();
+      }
+      $(".city-input-text").val("");
+    } catch (error) {
+      console.log("Error: " + error);
+    }
+  })();
 }
